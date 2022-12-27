175003ffb5200f0fbf693179da261563ebcdb0eb
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-12-27 20:17:55 +0100

    add remove method to classCartProduct

--- src/js/script.js
+++ src/js/script.js
@@ -454,6 +454,7 @@
       
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
       
       //console.log(thisCartProduct);
     }
@@ -478,6 +479,34 @@
         thisCartProduct.amount = thisCartProduct.amountWidget.value;
         thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+      });
+    }
+
+    remove(){
+      const thisCartProduct = this;
+
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
+
+    initActions(){
+      const thisCartProduct = this;
+
+      // thisCartProduct.dom.edit.addEventListener('click', function(){
+      //   event.preventDefault();
+      //   thisCartProduct.edit();
+      // });
+
+      thisCartProduct.dom.remove.addEventListener('click', function(){
+        event.preventDefault();
+        thisCartProduct.remove();
+        console.log('remove click');
       });
     }
   }
